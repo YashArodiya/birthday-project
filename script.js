c3ef9e8ee4fa4df7f5230eff09e3b98c5a00a42a
Yash Arodiya <88681035+YashArodiya@users.noreply.github.com>
2024-01-22 00:05:49 +0530

    Update script.js
    made changes so that time works correctly thanks to gpt and baisnabi

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-const currDate = new Date() ; 
-// console.log(currDate) 
 const myDate = new Date('3/4/2024') ;
-const timediff =  myDate - currDate ;
-// console.log("Time difference is " + timediff) 
-
 function func() {
     const currDate = new Date() ;
     const timediff =  myDate - currDate ;
-    const daysdiff = Math.ceil(timediff / (1000*60*60*24)) ; 
-    const hoursdiff = Math.ceil((timediff / (1000*60*60))%24) ; 
-    const minutesdiff = Math.ceil((timediff / (1000*60))%60) ; 
-    const secondsdiff = Math.ceil((timediff / (1000))%60) ; 
+    const daysdiff = Math.floor(timediff / (1000 * 60 * 60 * 24));
+    const hoursdiff = Math.floor(timediff / (1000 * 60 * 60)) % 24;
+    const minutesdiff = Math.floor((timediff / (1000 * 60)) % 60);
+    const secondsdiff = Math.floor((timediff / 1000) % 60);
     // console.log("Days: " + daysdiff) ; 
     // console.log("Hours: " + hoursdiff) ; 
     // console.log("Minutes: " + minutesdiff) ; 
